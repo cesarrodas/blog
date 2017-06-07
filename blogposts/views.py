f3dd9f1249c6b62910a5111977abb68043ed5e67
Cesar Rodas <rodascesar03@yahoo.com>
2017-06-07 12:19:49 -0400

    added posting support for html templates

--- blogposts/views.py
+++ blogposts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def home(request):
-    return render(request, 'blogposts/home.html')
+    Blog_Posts = BlogPost.objects.order_by('-pub_date')
+    return render(request, 'blogposts/home.html', {'BlogPosts': Blog_Posts})
